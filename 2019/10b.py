4b520ea2fa6c44791d361c64cc064d9960e9da26
Filip Simek <fsimek1@bloomberg.net>
2019-12-10 09:06:54 +0000

    Remove unnecessary code from day 10 / 2019

--- 2019/10b.py
+++ 2019/10b.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import math
-import sys
 
 grid = []
 with open('input10.txt', 'rt') as f:
@@ -22,42 +21,6 @@ for y, line in enumerate(grid):
 print(width, height)
 print("Total asteroids:", len(ast))
 
-def gcd(a, b):
-    a = abs(a)
-    b = abs(b)
-    while a != b:
-        s = min(a, b)
-        t = max(a, b)
-        if s == 0: return t
-        a = t-s
-        b = s
-    return a
-
-others = [x for x in ast if x != station]
-others.sort(key=lambda o: abs(o[0]-station[0]) + abs(o[1]-station[1]))
-obscured = set()
-
-def get_line(other):
-    if other in obscured:
-        return
-    x, y = other
-    dx = x - station[0]
-    dy = y - station[1]
-    g = gcd(dx, dy)
-    dx //= g
-    dy //= g
-    x += dx
-    y += dy
-    while x < width and x >= 0 and y < height and y >= 0:
-        yield (x, y)
-        x += dx
-        y += dy
-
-
-for o in others:
-    for x in get_line(o):
-        obscured.add(x)
-
 
 def angle(other):
     dx = other[0] - station[0]
@@ -67,11 +30,12 @@ def angle(other):
         a += 2 * math.pi
     return (a, dx*dx + dy*dy)
 
+
+others = [x for x in ast if x != station]
 others.sort(key=lambda o: angle(o))
-#for o in others:
-#    print(o, angle(o))
 
 counter = 1
+the200 = None
 while others:
     prevang = None
     erase = set()
@@ -81,7 +45,10 @@ while others:
             continue
         prevang = ang
         print("Elim", counter, o)
+        if counter == 200:
+            the200 = o
         erase.add(o)
         counter += 1
     others = [o for o in others if o not in erase]
 
+print("The 200th one was", the200)
