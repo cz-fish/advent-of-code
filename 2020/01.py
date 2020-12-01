3a3287924561f58cd4a7a609113fc507eb53080d
Filip Simek <fsimek1@bloomberg.net>
2020-12-01 08:42:58 +0000

    Optimize solution 2020/01

--- 2020/01.py
+++ 2020/01.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3.8
 
+from collections import defaultdict
+from functools import reduce
 import sys
 
 with open('input01.txt', 'rt') as f:
@@ -23,12 +25,42 @@ def part1():
 def part2():
     for i, a in enumerate(num):
         for j, b in enumerate(num[i+1:]):
-            for k, c in enumerate(num[j+1:]):
+            for c in num[j+1:]:
                 if a + b + c == 2020:
                     print(f"[{a}, {b}, {c}] -> {a*b*c}")
                     return
     print("No triplet found")
 
 
-part1()
-part2()
+# part1()
+# part2()
+
+
+def components(num, n, target):
+    """Assuming that there is only 1 way of adding `n` members
+    of `num` to get `target`"""
+    d = defaultdict(list)
+    for x in num:
+        if x > target:
+            continue
+
+        p = defaultdict(list)
+        for val, comps in d.items():
+            tval = val + x
+            if tval > target:
+                continue
+            for comp in comps:
+                tcomp = comp + [x]
+                if len(tcomp) == n:
+                    if tval == target:
+                        print(f"{tcomp} -> {reduce(lambda x, y: x * y, tcomp)}")
+                else:
+                    p[tval] += [tcomp]
+
+        d[x] += [[x]]
+        for k, v in p.items():
+            d[k] += v
+
+
+components(num, 2, 2020)
+components(num, 3, 2020)
