87eedcb82a398d6f45f8700591baef888f0c6e08
Filip Simek <filip.simek@gmail.com>
2020-12-14 01:03:59 +0000

    Add Modular arithmetic module, use it in 2020/13

--- 2020/13.py
+++ 2020/13.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.8
 
-from aoc import Env
+from aoc import Env, Modular
 
 e = Env(13)
 e.T("""939
@@ -48,55 +48,10 @@ e.run_main(1, part1)
 
 
 # --- Part 2---
-def chinese_remainders(buses):
-
-    def bezout(num, base):
-        # Extended Euclidean algorithm
-        s = 0
-        s_prev = 1
-        t = 1
-        t_prev = 0
-        r = base
-        r_prev = num
-        while r != 0:
-            q = r_prev // r
-            r_prev, r = r, r_prev - q * r
-            s_prev, s = s, s_prev - q * s
-            t_prev, t = t, t_prev - q * t
-        return s_prev, t_prev
-
-    a1 = 0
-    # First equation
-    # x = 0 (mod <first bus>)
-    # N1 is the product of all bus numbers folded so far
-    N1 = buses[0][0]
-    # For all remaining buses, one by one
-    for n2, a2neg in buses[1:]:
-        # Next equation
-        # x = -offset (mod <i-th bus>)
-        # `n2` is the i-th bus number
-        # `a2neg` is the time offset. Because this bus arrives
-        # at time `x + offset`, the right side is `-offset`.
-        a2 = -a2neg
-        # Determine Bezout coefficients for the i-th equation and
-        # the product of all equations before i.
-        m1, m2 = bezout(N1, n2)
-        # Chinese remainder theorem:
-        # x = a1 * m2 * n2 + a2 * M1 * N1
-        x = a1 * m2 * n2 + a2 * m1 * N1
-        # There are infinitely many `x`. We want the one between
-        # 0 and N1 - hence modulo
-        N1 *= n2
-        x = x % N1
-        # In the next iteration, x becomes the coefficient of the folded term
-        a1 = x
-    # The last x is the result
-    return a1
-
-
 def part2(input):
     _, buses = parse_input(input)
-    return chinese_remainders(buses)
+    coef = [(-offset, bus) for bus, offset in buses]
+    return Modular.chinese_remainders(coef)
 
 
 e.run_tests(2, part2)
--- 2020/aoc/__init__.py
+++ 2020/aoc/__init__.py
@@ -1,3 +1,4 @@
 from aoc.env import Env
 from aoc.grid import Grid
 from aoc.input import Input
+from aoc.modular import Modular
