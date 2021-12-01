7dc2a8f23a347dd4aa5240677ad00469af4e0539
Filip Simek <fsimek1@bloomberg.net>
2021-12-01 17:43:14 +0000

    Day 01 / 2021 in F#

---
+++ 2021-fs/aoc2021.fs
@@ -0,0 +1,32 @@
+// Learn more about F# at http://fsharp.org
+
+open System
+open AoC2021
+
+[<EntryPoint>]
+let main argv =
+    printfn "AoC 2021"
+    
+    let days = dict [
+        ("01a", Day01.partA);
+        ("01b", Day01.partB);
+    ]
+
+    let mutable dayFunction = fun() -> 1
+
+    let found =
+        if argv.Length = 0 then
+            printfn "Required parameter - day"
+            false
+        else
+            days.TryGetValue(argv.[0], &dayFunction)
+
+    if found then
+        dayFunction()
+    else
+        printfn "Available parameters: %A" days.Keys
+        1
+
+    (* printfn "Press enter to close"
+       System.Console.ReadLine() |> ignore *)
+       
