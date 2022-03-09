b445dd86dc0a6c570ae89d54e34a88225d1206ae
Filip Simek <filip.simek@gmail.com>
2022-03-09 20:39:48 +0000

    Day 16, 2018 part 2

--- 2018/16.py
+++ 2018/16.py
@@ -2,7 +2,7 @@
 
 from aoc import Env
 import re
-from typing import Tuple
+from typing import Dict, List, Tuple
 
 e = Env(16)
 
@@ -65,6 +65,13 @@ class WristwatchComputer:
             assert False, f"Invalid instruction {instruction}"
 
         self.reg[dst] = val
+    
+    def run(self, program: List[Tuple[int, int, int, int]], instr_map: Dict[int, str]):
+        for instruction in program:
+            opnum, s1, s2, dst = instruction
+            assert opnum in instr_map
+            opcode = instr_map[opnum]
+            self.step((opcode, s1, s2, dst))
 
 
 def parse_observed(group):
@@ -86,25 +93,28 @@ def parse_input(input):
     observed = []
     for i in range(len(groups) - 1):
         observed.append(parse_observed(groups[i]))
-    return observed, groups[-1]
+    program = []
+    for ln in groups[-1]:
+        program.append(tuple([int(x) for x in ln.split(' ')]))
+    return observed, program
 
 
-def num_opcodes(observed):
+def which_opcodes(observed):
     before, instr, after = observed
-    num = 0
+    possible = []
     for opcode in WristwatchComputer.all_instructions():
         comp = WristwatchComputer()
         comp.reg = list(before)
         comp.step((opcode, instr[1], instr[2], instr[3]))
         if comp.reg == list(after):
-            num += 1
-    return num
+            possible.append(opcode)
+    return possible
 
 
 def part1(input):
     observed, program = parse_input(input)
     # print(f"Total groups: {len(observed)}")
-    return sum([1 for o in observed if num_opcodes(o) >= 3])
+    return sum([1 for o in observed if len(which_opcodes(o)) >= 3])
 
 
 # == Tests ==
@@ -115,7 +125,7 @@ def test_parse_observed():
 
 def test_possible_opcodes():
     expected = 3
-    actual = num_opcodes(parse_observed(example))
+    actual = len(which_opcodes(parse_observed(example)))
     assert actual == expected, f"Fail: expected {expected}, actual {actual}"
 
 
@@ -127,10 +137,38 @@ e.run_tests(1, part1)
 e.run_main(1, part1)
 
 
+def map_instruction_numbers(observed):
+    all_ops = WristwatchComputer.all_instructions()
+    mapping = {i: None for i in range(len(all_ops))}
+    undecided = set([x for x in all_ops])
+    possible = {}
+    for o in observed:
+        opnum = o[1][0]
+        if mapping[opnum] is not None:
+            # this opnum is already assigned to an opcode
+            continue
+        alter = set([a for a in which_opcodes(o) if a in undecided])
+        if opnum in possible:
+            alter = alter.intersection(possible[opnum])
+        assert len(alter) > 0
+        if len(alter) == 1:
+            mapping[opnum] = alter.pop()
+            undecided.remove(mapping[opnum])
+        else:
+            possible[opnum] = alter
+    assert None not in mapping.values()
+    return mapping
+
+
 def part2(input):
-    pass
+    observed, program = parse_input(input)
+    mapping = map_instruction_numbers(observed)
+    for k, v in mapping.items():
+        print(f"{k} -> {v}")
+    comp = WristwatchComputer()
+    comp.run(program, mapping)
+    return comp.reg[0]
 
 
-# e.run_tests(2, part2)
-# e.run_main(2, part2)
+e.run_main(2, part2)
 
