919acfd8c0ab7e8cb6964c9fffcfecac91a34f74
Filip Simek <filip.simek@gmail.com>
2022-11-28 08:34:15 +0000

    Fix IDE failure

--- 2015/template/0.test.js
+++ 2015/template/0.test.js
@@ -1,7 +1,6 @@
 const { expect } = require("@jest/globals");
-const { TestWatcher } = require("jest");
 const d0 = require("./0.js");
 
-TestWatcher('part1 test example', () => {
+test('part1 test example', () => {
     expect(d0.part1()).toBe(1);
 });
