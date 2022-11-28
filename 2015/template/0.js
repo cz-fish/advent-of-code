14181b31bba5dc70249cf4b7ae6c5dfc42f04ee2
Filip Simek <filip.simek@gmail.com>
2022-11-28 08:13:31 +0000

    Make template day 0

---
+++ 2015/template/0.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function part1() {
+}
+
+function part2() {
+}
+
+function run() {
+    const data = fs.readFileSync('./input0.txt', 'utf8');
+
+    const res_part1 = part1(data);
+    console.log(`Part 1 result ${res_part1}`);
+    const res_part2 = part2(data);
+    console.log(`Part 2 result ${res_part2}`);
+}
+
+exports.run = run;
+exports.part1 = part1;
+exports.part2 = part2;
+
+if (require.main === module) {
+    run();
+}
