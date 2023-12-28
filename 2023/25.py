1e83dc5a683b59b336fc8981fd2230a285e62236
Filip Simek <filip.simek@gmail.com>
2023-12-28 09:29:39 +0000

    Add slow algorithmic solution for day 25 / 2023

--- 2023/25.py
+++ 2023/25.py
@@ -2,6 +2,7 @@
 
 from aoc import Env
 from collections import defaultdict, deque
+import heapq
 
 e = Env(25, param="25.dot")
 e.T("""jqt: rhn xhk nvd
@@ -33,6 +34,29 @@ def parse_input(input):
     return graph, dot
 
 
+def find_cuts(graph):
+    counters = defaultdict(int)
+    for start in graph.keys():
+        q = deque([start])
+        prev_nodes = {start: None}
+        while q:
+            pos = q.popleft()
+            for nei in graph[pos]:
+                if nei not in prev_nodes:
+                    prev_nodes[nei] = pos
+                    q.append(nei)
+        for node0, node1 in prev_nodes.items():
+            if node1 is None:
+                continue
+            edge = (min(node0, node1), max(node0, node1))
+            counters[edge] += 1
+    sorted_edges = []
+    for edge, usage in counters.items():
+        heapq.heappush(sorted_edges, (-usage, edge))
+    assert len(sorted_edges) >= 3
+    return [heapq.heappop(sorted_edges)[1] for _ in range(3)]
+
+
 def count_nodes(graph, start):
     visited = set([start])
     q = deque([start])
@@ -51,8 +75,10 @@ def part1(input):
     #with open(fname, "wt") as f:
     #    print(dot, file=f)
 
-    # 3 cuts found by rendering the graph with the neato engine of graphviz
-    cuts = [('tqr', 'grd'), ('tqh', 'dlv'), ('ngp', 'bmd')]
+    ## 3 cuts found by rendering the graph with the neato engine of graphviz
+    #cuts = [('tqr', 'grd'), ('tqh', 'dlv'), ('ngp', 'bmd')]
+    cuts = find_cuts(graph)
+
     for first, second in cuts:
         assert second in graph[first], print(f"{first}, {second}")
         graph[first].remove(second)
@@ -60,7 +86,7 @@ def part1(input):
     return count_nodes(graph, cuts[0][0]) * count_nodes(graph, cuts[0][1])
 
 
-#e.run_tests(1, part1)
+e.run_tests(1, part1)
 e.run_main(1, part1)
 
 
