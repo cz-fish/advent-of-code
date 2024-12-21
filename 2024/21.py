640e816024bbd34c3cb715a0906891d3641df855
Filip Simek <filip.simek@gmail.com>
2024-12-21 15:26:15 +0000

    Day 21 / 2024, part 2

--- 2024/21.py
+++ 2024/21.py
@@ -150,6 +150,7 @@ def apply_robot(instr, is_numpad):
 
 
 g_cache = {}
+g_top_cache = {}
 
 def numeric_code(code, intermediate_robots):
     coords = {
@@ -166,22 +167,20 @@ def numeric_code(code, intermediate_robots):
         'A': (2, 3),
     }
     x, y = coords['A']
-    out = []
+    out = 0
     for key in code:
         tx, ty = coords[key]
         dx = tx - x
         dy = ty - y
-        out.append(one_move(intermediate_robots, x, y, dx, dy, (0, 3)))
+        out += one_move(intermediate_robots, x, y, dx, dy, (0, 3))
         x = tx
         y = ty
-    return ''.join(out)
+    return out
 
 
 def one_move(level, sx, sy, dx, dy, avoid):
-    if (level, sx, sy, dx, dy) in g_cache:
-        return g_cache[(level, sx, sy, dx, dy)]
+    cache_key = (level, dx, dy, sx, sy)
     options = []
-
     def move_x(d):
         if d < 0: # left
             return '<' * abs(d)
@@ -196,8 +195,12 @@ def one_move(level, sx, sy, dx, dy, avoid):
 
     if dy == 0:
         options.append(move_x(dx) + 'A')
+        # sx, sy coordinates don't matter for cache
+        cache_key = (level, dx, dy, 0, 0)
     elif dx == 0:
         options.append(move_y(dy) + 'A')
+        # sx, sy coordinates don't matter for cache
+        cache_key = (level, dx, dy, 0, 0)
     else:
         # either upply dx first and then dy, or vice versa
         tx = sx + dx
@@ -208,18 +211,25 @@ def one_move(level, sx, sy, dx, dy, avoid):
         if (sx, ty) != avoid:
             # apply first y then x
             options.append(move_y(dy) + move_x(dx) + 'A')
+        if len(options) == 2:
+            # sx, sy coordinates don't matter for cache
+            cache_key = (level, dx, dy, 0, 0)
     assert options, f"There are no options to move from {sx},{sy} by {dx},{dy} and avoid {avoid}"
+
+    cache = g_cache if avoid == (0, 0) else g_top_cache
+    if cache_key in cache:
+        return cache[cache_key]
 
     shortest = None
     if level == 0:
         options.sort(key=lambda x: len(x))
-        shortest = options[0]
+        shortest = len(options[0])
     else:
         for opt in options:
             expanded = next_robot_step(level - 1, opt)
-            if shortest is None or len(expanded) < len(shortest):
+            if shortest is None or expanded < shortest:
                 shortest = expanded
-    g_cache[(level, sx, sy, dx, dy)] = shortest
+    cache[cache_key] = shortest
     return shortest
 
 
@@ -232,26 +242,26 @@ def next_robot_step(level, instr):
         '>': (2, 1),
     }
     x, y = coords['A']
-    out = []
+    out = 0
     for key in instr:
         tx, ty = coords[key]
         dx = tx - x
         dy = ty - y
-        out.append(one_move(level, x, y, dx, dy, (0, 0)))
+        out += one_move(level, x, y, dx, dy, (0, 0))
         x = tx
         y = ty
-    line = ''.join(out)
-    #print(level, line)
-    return line
+    return out
 
 
 def part1(input):
     global g_cache
+    global g_top_cache
     g_cache = {}
+    g_top_cache = {}
     total = 0
     for code in input.get_valid_lines():
         seq = numeric_code(code, 2)
-        v = len(seq) * int(code[:-1])
+        v = seq * int(code[:-1])
         total += v
     return total
 
@@ -276,14 +286,16 @@ v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A
 
 def part2(input):
     global g_cache
+    global g_top_cache
     g_cache = {}
+    g_top_cache = {}
     total = 0
     for code in input.get_valid_lines():
         seq = numeric_code(code, 25)
-        v = len(seq) * int(code[:-1])
+        v = seq * int(code[:-1])
         total += v
     return total
 
 
-#e.run_tests(2, part2)
-#e.run_main(2, part2)
+e.run_tests(2, part2)
+e.run_main(2, part2)
