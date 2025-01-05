502ccc058faa84b47c8640875b53d69c26b94efd
Filip Simek <filip.simek@gmail.com>
2025-01-05 11:53:18 +0000

    2015 / 19, part 2, including wrong approaches

--- 2015/19.py
+++ 2015/19.py
@@ -3,6 +3,7 @@
 from pyaoc import Env
 
 from collections import defaultdict, deque
+import heapq
 import re
 
 
@@ -121,6 +122,30 @@ def get_terminals(subs, formula):
 
 
 def make_rule_map(rules):
+    firsts = {}
+    for left in rules.keys():
+        possible_first = {}
+        q = deque()
+        for rule in rules[left]:
+            tokens, rule_nr = rule
+            q.append((tokens[0], rule_nr))
+        while q:
+            head, first_rule = q.popleft()
+            if head in possible_first:
+                possible_first[head].add(first_rule)
+            else:
+                possible_first[head] = set([first_rule])
+                if head in rules:
+                    for next_rule in rules[head]:
+                        tokens, _ = next_rule
+                        q.append((tokens[0], first_rule))
+        firsts[left] = possible_first
+    #for k, v in firsts.items():
+    #    print(f"{k} ->")
+    #    for o, r in v.items():
+    #        print(f"      {o} via {r}")
+    return firsts
+"""
     # map: for each left side - a list of different possible right side expansions
     #      for each expansion: (the first atom, the rest, and number of steps, list of numbers of rules applied)
     rule_map = {}
@@ -152,10 +177,97 @@ def make_rule_map(rules):
     for left, firsts in transitive_map.items():
         print(f"Firsts of {left}: {list(firsts)}")
     return rule_map
+"""
 
 
-def count_rule_expansions(rule_map, formula):
-    return 0
+def check_and_append(q, seen, formula, exp, depth, pos):
+    if len(exp) > len(formula):
+        return
+    if pos == len(formula) and exp:
+        return
+    #heapq.heappush(q, (depth, depth, exp, pos))
+    key = (pos, tuple(exp))
+    if key in seen:
+        return
+    seen.add(key)
+    q.append((exp, depth, pos))
+
+
+def count_rule_expansions(rule_map, rule_by_nr, formula):
+    q = deque()
+    # current expansion, depth, formula_pos
+    q.append((["e"], 0, 0))
+    #q = []
+    # -pos, depth, expansion, pos
+    #heapq.heappush(q, (0, 0, ["e"], 0))
+    best = 0
+    counter = 0
+    seen = set()
+    while q:
+        exp, depth, pos = q.popleft()
+        #_, depth, exp, pos = heapq.heappop(q)
+        counter += 1
+        best = max(best, pos)
+        if counter % 10000 == 0:
+            print(f"{counter}: best pos {best}, depth {depth}, q len {len(q)}")
+        if pos == len(formula):
+            if not exp:
+                # solution reached
+                return depth
+            # over-expanded
+            continue
+        if not exp:
+            # exhausted all expansion, but no solution matched
+            continue
+        if len(exp) >= len(formula):
+            # already too long, no need to continue, as every
+            # expansion rule only makes the formula longer
+            continue
+        first = exp[0]
+        peek = formula[pos]
+        if first == peek:
+            # possible to consume one character
+            #q.append((exp[1:], depth, pos + 1))
+            check_and_append(q, seen, formula, exp[1:], depth, pos + 1)
+        if first not in rule_map or peek not in rule_map[first]:
+            # not possible to expand
+            continue
+        # expand all potentially perspective rules
+        for rule_nr in rule_map[first][peek]:
+            rule_tokens = rule_by_nr[rule_nr]
+            #q.append((rule_tokens + exp[1:], depth + 1, pos))
+            check_and_append(q, seen, formula, rule_tokens + exp[1:], depth + 1, pos)
+
+    assert False, "Solution not found"
+
+
+def reduce_formula(formula, rules):
+    q = []
+    best = {}
+    heapq.heappush(q, (len(formula), 0, formula))
+    counter = 0
+    shortest = None
+    while q:
+        size, depth, f = heapq.heappop(q)
+        if f == 'e':
+            break
+        if shortest is None or size < shortest:
+            shortest = size
+        counter += 1
+        if counter % 1000 == 0:
+            print(f"{counter} {size} / {shortest}, {len(q)}")
+        for l, rights in rules.items():
+            for r in rights:
+                i = f.find(r)
+                if i == -1:
+                    continue
+                nf = f[:i] + l + f[i+len(r):]
+                if nf in best:
+                    if best[nf] <= depth + 1:
+                        continue
+                best[nf] = depth + 1
+                heapq.heappush(q, (len(nf), depth + 1, nf))
+    return best['e']
 
 
 def part2(full_input):
@@ -164,14 +276,17 @@ def part2(full_input):
     # tokenize all rules, and give them all unique numbers
     # Assumption: all formulas made up of atoms
     #    each atom starts with capital letter -> tokenization
-    rules = {}
+    rule_by_left = {}
+    rule_by_id = {}
     counter = 0
     for left, rights in subs.items():
         rules_of_left = []
         for right in rights:
-            rules_of_left.append((tokenize(right), counter))
+            tokenized = tokenize(right)
+            rules_of_left.append((tokenized, counter))
+            rule_by_id[counter] = tokenized
             counter += 1
-        rules[left] = rules_of_left
+        rule_by_left[left] = rules_of_left
     token_formula = tokenize(formula)
 
     # Find terminals and non-terminals -> not really that useful because the formula doesn't only contain
@@ -184,28 +299,13 @@ def part2(full_input):
 
     # Construct First and Follow sets
     # map: for each non-terminal a list of firsts. For each first in the list - full expansion from the non-terminal and number of steps
-    rule_map = make_rule_map(rules)
+    #rule_map = make_rule_map(rule_by_left)
 
     # Parse formula using prefix matching. We should be able to accumulate number of rules applied as we go
-    return count_rule_expansions(rule_map, token_formula)
+    #return count_rule_expansions(rule_map, rule_by_id, token_formula)
+
+    return reduce_formula(formula, subs)
 
 
 e.run_tests(2, part2)
 e.run_main(2, part2)
-
-
-"""
-    for test_case, exp1, exp2 in test_inputs:
-        res = part1(test_case)
-        print(f"Test case {num}, part 1, expected {exp1} got {res} {'not a ' if res != exp1 else ''}match")
-        res = part2(test_case)
-        print(f"Test case {num}, part 2, expected {exp2} got {res} {'not a ' if res != exp2 else ''}match")
-        num += 1
-    # actual input
-    print(f"Actual input, part 1: {part1(main_input)}")
-    print(f"Actual input, part 2: {part2(main_input)}")
-
-
-if __name__ == "__main__":
-    main()
-"""
